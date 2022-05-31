601f6e655bbc04b39ce78a12607e0c422431a9b2
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2022-05-31 12:32:52 +0200

    first commit

---
+++ src/main/java/com/team7/matadorenbackend/MatadorenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.team7.matadorenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatadorenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatadorenBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team7/matadorenbackend/MatadorenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team7.matadorenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatadorenBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
